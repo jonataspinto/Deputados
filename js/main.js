664c048094ec68c8e4a13d9e902bcf7d298779aa
Jonatas <jonatastdb_jp@hotmail.com>
2019-08-22 00:01:00 -0300

    partidos --WIP

--- js/main.js
+++ js/main.js
@@ -1,40 +1,3 @@
-const req = (url)=>{
-    fetch(url).then(res=> res.json()).then(res =>{
-        // console.log(res.dados)
-        // console.log(res.dados[0].urlFoto)
-        mountCard(res)
-    }).catch(res=> {console.log(res)})
-}
-
-const mountCard = (content)=>{
-    // console.log(content)
-    for(let i = 0; i< content.dados.length; i++) {
-        // console.log(content.dados[i].nome)
-        // let date = new Date(element.commit.author.date)
-        let ins = `
-
-        <aside class="col s12 m6 l4 ">
-          <div class="card">
-            <div class="card-image">
-              <img src="${content.dados[i].urlFoto}" style="width: 40%">
-              </div>
-              <div class="card-content">
-              <span class="card-title">${content.dados[i].nome}</span>
-              <p>${content.dados[i].email}</p>
-            </div>
-            <div class="card-action">
-              <a href="#">ID: ${content.dados[i].id}</a>
-            </div>
-          </div>
-      </aside>
-        `
-    document.getElementById('container').innerHTML += ins
-        
-    };
-}
-
-req("https://dadosabertos.camara.leg.br/api/v2/deputados?pagina=2&itens=12")
-
 $('.dropdown-button').dropdown({
   inDuration: 100,
   outDuration: 25,
@@ -47,3 +10,47 @@ $('.slider').slider();
 $('.tooltipped').tooltip();
 $('.modal').modal();
 });
+
+const getdeputados = (url)=>{
+    fetch(url).then(res=> res.json()).then(res =>{
+        montaCard(res)
+        // console.log(res.dados[1])
+    }).catch(res=> {console.log(res)})
+}
+
+const getpartidos = (url)=>{
+    fetch(url).then(res=> res.json()).then(res =>{
+        // console.log(res.dados)
+    }).catch(res=> {console.log(res)})
+}
+
+
+const montaCard = (content)=>{
+  for(let i = 0; i< content.dados.length; i++) {
+      let card = `
+      <aside class="col s12 m6 l4 ">
+        <div class="card">
+          <div class=" card horizontal center-align">
+            <img src="${content.dados[i].urlFoto}" style="width: 40%">
+            <h5 class="card-content" >${content.dados[i].siglaPartido}<h5/>
+          </div>
+          <div class="card-content">
+            <span class="card-title">${content.dados[i].nome}</span>
+            <p>${content.dados[i].email}</p>
+          </div>
+          <div class="card-action">
+            <a href="#">ID: ${content.dados[i].id}</a>
+          </div>
+        </div>
+      </aside>
+      `
+  document.getElementById('deputados').innerHTML += card        
+  };
+}
+
+getdeputados("https://dadosabertos.camara.leg.br/api/v2/deputados?pagina=2&itens=12")
+
+getpartidos("https://dadosabertos.camara.leg.br/api/v2/partidos")
+
+
+
